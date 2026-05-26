d5b4b1cebe596051c95421cb3c7c0f209d8d4795
taufik50ahmad <taufik50ahmad@gmail.com>
2026-05-26 14:02:20 +0700

    Added CORS and Express JSON

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import "dotenv/config"
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-  
+
+app.use(cors({
+  origin: ["http://localhost:5173"]
+}))
+
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
